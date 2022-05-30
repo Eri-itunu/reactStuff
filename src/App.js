6562a0b5e07be15fa7cd7b22bc83e1f5601c5bc5
Eriitunu <adesioyeeriitunu@gmail.com>
2022-05-30 15:12:23 +0100

    new new

---
+++ src/App.js
@@ -0,0 +1,8 @@
+export default function App(){
+    return(
+        <div>
+            <h1>What is up</h1>
+            <h2>Hello</h2>
+        </div>
+    )
+}
---
+++ src/Footer.js
@@ -0,0 +1,8 @@
+export default function Footer(){
+    return(
+        <footer>
+            <small>  @ 2021 Adesioye development. All rights reserved</small>
+        </footer>
+    )
+}
+
